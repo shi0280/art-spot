0f9909cb550717e13440d3b04dd8cb3b10b2e072
shi0280 <sho_ko504128@yahoo.co.jp>
2020-07-24 17:54:17 +0900

    add-user-function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  #before_action :move_to_index, only: [:show, :edit]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -7,4 +7,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  #def move_to_index
+    #unless user_signed_in?
+    # redirect_to action: :index
+    #end
+  #end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
+  resources :users, only: [:show, :edit, :update]
 end
