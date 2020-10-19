84cec736b35dd2e55388dc2e4dc65babb9658a3d
shi0280 <sho_ko504128@yahoo.co.jp>
2020-10-19 23:22:30 +0900

    edit-model

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,7 +1,7 @@
 class Spot < ApplicationRecord
-  has_many :spot_users
-  has_many :users, through: :spot_users
-  has_many :posts
+  has_many :spot_users, dependent: :destroy
+  has_many :users, through: :spot_users, dependent: :destroy
+  has_many :posts, dependent: :destroy
 
   mount_uploader :image, ImageUploader
 end
